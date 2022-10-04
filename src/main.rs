4e146dccecad0df57f42ff69d0ed8cf4000a99f7
Ori Sky Farrell <ori.sky.farrell+git@gmail.com>
2022-10-04 02:34:13 +0100

    Refactor code

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,10 @@ use uefi::table::boot::*;
 
 #[entry]
 fn entry(handle: Handle, system_table: SystemTable<Boot>) -> Status {
-    main(handle, system_table).status()
+    boot(handle, system_table).status()
 }
 
-fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> uefi::Result {
+fn boot(handle: Handle, mut system_table: SystemTable<Boot>) -> uefi::Result {
     uefi_services::init(&mut system_table)?;
 
     let stdout = system_table.stdout();
@@ -22,30 +22,26 @@ fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> uefi::Result {
     stdout.set_color(Color::Black, Color::Green)?;
     stdout.clear()?;
 
+    uefi_services::println!("Entered UEFI boot services");
+    uefi_services::println!("Welcome to yuzu 0.0.1");
+
     let revision      = system_table.firmware_revision();
     let uefi_revision = system_table.uefi_revision();
 
-    uefi_services::println!("Welcome to yuzu 0.0.1");
     uefi_services::println!("Firmware: {} {}.{} (UEFI {}.{})",
                             system_table.firmware_vendor(),
                             revision.major(), revision.minor(),
                             uefi_revision.major(), uefi_revision.minor());
 
-    let boot_services = system_table.boot_services();
-
-    let mut status;
     let mut buf = Vec::new();
+    let mut status = Status::BUFFER_TOO_SMALL;
 
-    while {
-        let buf = &mut buf;
-
+    while status == Status::BUFFER_TOO_SMALL {
+        let boot_services = system_table.boot_services();
         let MemoryMapSize { map_size, .. } = boot_services.memory_map_size();
-
         buf.resize(map_size, 0);
-
-        status = boot_services.memory_map(buf).status();
-        status == Status::BUFFER_TOO_SMALL
-    } {}
+        status = boot_services.memory_map(&mut buf).status();
+    }
 
     uefi::Result::from(status)?;
 
@@ -54,5 +50,10 @@ fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> uefi::Result {
 
     system_table.exit_boot_services(handle, &mut buf)?;
 
+    runtime()
+}
+
+fn runtime() -> uefi::Result {
+    uefi_services::println!("Entered UEFI runtime services");
     loop {}
 }
